fe9ad89a26ca5b78303e40033422115ec5087126
Daniel Ikoyo <dannirolands@gmail.com>
2024-02-05 19:00:52 +0100

    fix private route

--- src/utils/isRoutePrivate.ts
+++ src/utils/isRoutePrivate.ts
@@ -1,14 +1,21 @@
-export const isRoutePrivate = (route: string, protectedRoute: string[]) => {
+const ROOT_ROUTE = "/";
+
+export const isRoutePrivate = (route: string, protectedRoutes: string[]) => {
   if (isRootRoute(route)) {
     return true;
   }
-  if (!isRootRoute(route) && protectedRoute.includes(route)) {
-    return true;
-  }
 
-  return false;
+  const isProtected =
+    protectedRoutes
+      .map((protectedRoute) => {
+        if (isRootRoute(protectedRoute)) return false;
+        return route.includes(protectedRoute);
+      })
+      .filter(Boolean).length > 0;
+
+  return isProtected;
 };
 
 const isRootRoute = (route: string) => {
-  return route === "/";
+  return route === ROOT_ROUTE;
 };
